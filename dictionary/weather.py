3202f6fa8509f76b8c32719fcb67fbd595406e44
praveen <praveensisupalan34@gmail.com>
2022-06-17 09:14:48 -0700

    none

---
+++ dictionary/weather.py
@@ -0,0 +1,41 @@
+weather=[
+    {"district": "tvm", "temp":30},
+    {"district": "ktm","temp":28 },
+    {"district": "apy","temp":27},
+    {"district": "idk","temp":18 },
+    {"district": "ekm","temp":31 },
+    {"district": "pta","temp":21},
+    {"district": "tsr","temp":24},
+    {"district": "kzd","temp":29},
+    {"district": "pkd","temp":34},
+    {"district": "mpm","temp":31},
+
+    {"district": "tvm", "temp": 31},
+    {"district": "ktm", "temp": 29},
+    {"district": "apy", "temp": 26},
+    {"district": "idk", "temp": 20},
+    {"district": "ekm", "temp": 30},
+    {"district": "pta", "temp": 22},
+    {"district": "tsr", "temp": 27},
+    {"district": "kzd", "temp": 28},
+    {"district": "pkd", "temp": 30},
+    {"district": "mpm", "temp": 29},
+
+]
+# out={"tvm":31,"ktm":29,}
+com_weather=[]
+
+for weth in weather:
+    if weth in com_weather:
+        if
+
+
+
+
+# sort out based on temparature
+
+# find max tem district
+
+# find min tem district
+
+# dictionary methods
