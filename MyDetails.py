6389aa2bc929df277043f418b8a574c433343a88
praveen <praveensisupalan34@gmail.com>
2022-06-23 21:48:57 +0530

    none

--- MyDetails.py
+++
@@ -1,5 +0,0 @@
-name="praveen"
-age="21"
-location="vandanam"
-
-print(f"Hai it's me {name} iam from {location} iam {age} years old")
--- flowcontrols/testdemo/demo.py
+++ flowcontrols/testdemo/demo.py
@@ -1,5 +1,3 @@
-from praveenpythonwork.flowcontrols.mathoperation.operation import *
-
 print (add(10,20))
 
 print(sub(2,5))
