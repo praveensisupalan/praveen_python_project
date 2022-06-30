fe03efd6c6eff8588fde388b9f119da68f87859e
praveen <praveensisupalan34@gmail.com>
2022-06-30 09:00:23 -0700

    none

--- Blog/views.py
+++ Blog/views.py
@@ -1,36 +1,57 @@
-from praveenpythonwork.Blog.models import users,posts
+from praveenpythonwork.Blog.models import users, posts
 
-session={}
+session = {}
 
-         # authenticate
 
+def SigninRequired(fn):
+    def wrapper(*args, **kwargs):
+        if "user" in session:
+            return fn(*args, **kwargs)
+        else:
+            return None
+
+    return wrapper
+
+
+# authenticate
 def authenticate(**kwargs):
-    username=kwargs.get("username")
-    password=kwargs.get("password")
+    username = kwargs.get("username")
+    password = kwargs.get("password")
 
-    user=[user for user in users  if user["username"]==username and user["password"]==password]
+    user = [user for user in users if user["username"] == username and user["password"] == password]
     return user
+
 
 class LoginViews:
 
-    def post(self,**kwargs):
-        username=kwargs.get("username")
-        password=kwargs.get("password")
-        user=authenticate(username=username,password=password)
+    def post(self, **kwargs):
+        username = kwargs.get("username")
+        password = kwargs.get("password")
+        user = authenticate(username=username, password=password)
 
         if user:
-            session["user"]=user[0]
-        print(session)
+            session["user"] = user[0]
+        # print(session)
 
 
 class PostListViews:
-
-    def get(self,**kwargs):
-
+    @SigninRequired
+    def get(self, *args, **kwargs):
         return posts
 
-login=LoginViews()
 
-login.post(username="akhil", password="Password@123")
-posts=PostListViews()
+class MyPostViews:
+    @SigninRequired
+    def get(self, *args, **kwargs):
+        userid=session["user"]["id"]
+        mypost=[post for post in posts if post["userId"]==userid]
+        return mypost
 
+login = LoginViews()
+login.post(username="richard", password="Password@123")
+
+# all_post = PostListViews()
+# print(all_post.get())
+
+mypost= MyPostViews()
+print(mypost.get())
