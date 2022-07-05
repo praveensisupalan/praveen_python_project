c6c0b9d437083ec864de48d6efcc0abd53bc4137
praveen <praveensisupalan34@gmail.com>
2022-07-05 11:38:08 +0530

    none

--- countries/countriesProcess.py
+++ countries/countriesProcess.py
@@ -1,26 +1,48 @@
 import json
 
-with open("countries.json", encoding="utf-8")as countries:
-    CountriesList=json.load(countries)
+with open("countries.json", encoding="utf-8") as countries:
+    CountriesList = json.load(countries)
+
+
+def GetCountryData(CountryName):
+    return [country for country in CountriesList if country["name"].lower().startswith(CountryName)]
+
 
 # print total number of country details
 
-print("total number of country details = ",len(CountriesList))
+print("total number of country details = ", len(CountriesList))
 
-# print languages of ukrane
+# print languages of Ukraine
 
-countryLan=[country["languages"] for country in CountriesList if country["name"]=="Afghanistan"]
+countryLan = GetCountryData("ukraine")
+lan = countryLan[0].get("languages")
+print(lan[0].get("name"))
 
-print([i["name"] for i in countryLan[0]])
-
-# print currency of china
-countryCurr=[country["currencies"] for country in CountriesList if country["name"]=="China"]
-print("currency of china= ",countryCurr[0][0]["name"])
+# print currency of Palestine
+countryCurr = GetCountryData("palestine")
+print([contry["name"] for contry in countryCurr["currencies"])
+# print("currency of Palestine = ", [curr["name"] for curr in countryCurr[0]])
 
 # print population of india
-countryPopul=[country["population"] for country in CountriesList if country["name"]=="India"]
-print(countryPopul)
+countryPopul = [country["population"] for country in CountriesList if country["name"] == "India"]
+print("population of india = ", countryPopul[0])
 
 # print nigehbouring countries of australia
-countryNig=[country["borders"] for country in CountriesList if country["name"]=="Afghanistan"]
-print(countryNig)
+countryNig = [country["borders"] for country in CountriesList if country["name"] == "Afghanistan"]
+print("nigehbouring countries of australia = ", countryNig[0])
+
+# print nigehbouring countries name of india
+
+indiaBorder = GetCountryData("india")
+borders = indiaBorder[0].get("borders")
+sharingBorders = [country.get("name") for country in CountriesList if country["alpha3Code"] in borders]
+print(sharingBorders)
+
+# max populated country
+maxPopulation = max(CountriesList, key=lambda country: country.get("population"))
+print(maxPopulation["name"])
+
+# min populated country
+
+mixPopulation = min(CountriesList, key=lambda country: country.get("population"))
+print(mixPopulation["name"])
